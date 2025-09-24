b022ac192b8640b1047fd117c14ab70d6d279b65
gaurav yadav <grvyadav76@gmail.com>
2025-09-25 01:50:20 +0530

    successfully send from client to server

---
+++ client.c
@@ -0,0 +1,67 @@
+//whats ai_protocol in soockfd()'s 3rd parameter , what u r supposed to pass their 
+//u haven't specified right error handling, nigga go do it 
+
+#define _POSIX_C_SOURCE 200112L
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>      //contains - struct addrinfo
+#include<arpa/inet.h> // For inet_pton
+#include<string.h> // For memset
+#include<unistd.h> // For close
+
+#define PORT "8080"
+#define SERVER_IP "127.0.0.1" //  server's(ur own system) loopback address
+
+
+int main(){
+
+    struct addrinfo inai,*servinfo;
+    
+    memset(&inai, 0, sizeof(inai));
+    inai.ai_family = AF_UNSPEC;     //both allowed ipv 4 and 6
+    inai.ai_socktype = SOCK_STREAM;
+
+    //to store server address information
+    if(getaddrinfo(SERVER_IP,PORT,&inai,&servinfo)!=0) {       //try localhost in 1st parameter
+        printf("getaddrinfo error\n");
+        return 1;
+    } 
+
+    printf("ip style : %d\n",servinfo->ai_family);
+    //printf("lets see : %d\n",servinfo->ai_next->ai_family);
+
+    //socket creation
+    int sockfd = socket(servinfo->ai_family,servinfo->ai_socktype,servinfo->ai_protocol);
+    if (sockfd < 0) {
+        perror("socket creation failed");
+        return 1;
+    }
+
+    printf("Client is ready to connect to %s:%s\n", SERVER_IP, PORT);
+
+    //connect to server address
+    if(connect(sockfd,servinfo->ai_addr,servinfo->ai_addrlen)!=0) {
+        close(sockfd);
+        printf("connection failed\n");
+        return 1;
+    }
+
+    printf("Connected successfully!\n");
+
+    //sending to server
+    if(send(sockfd,"ssup server",12,0)<0) {
+        perror("send error : ");
+        return 1;
+    }else {
+        printf("message sent successfully to server\n");
+    }
+
+    freeaddrinfo(servinfo);
+    close(sockfd);
+    return 0;
+}
+
+//(done)try local host in getaddrinfo()'s 1st parameter to see if it would go look for localhost address which is "127.0.0.1"
+//(done)whcih ip type gets selected when AF_UNSPEC gets use , how it chooses ? 
+//(done)when a already declared variable of struct addrinfo like inai (in our program) then passing through getaddrinfo() does it fills the inai variable with new things or creates new linked list which has nothing to do with inai and assigns list to pointer variable whichever is in its 4th parameter
---
+++ server.c
@@ -0,0 +1,117 @@
+
+//disclaimer - 
+//1. our program is made considering there can't be any other possibility than 2 linked list allocated by getaddrinfo() , one havng ipv4 and other ipv6
+//2. our program is using ipv6 for connection inside socket , its suppossed to handle both ipv4 and ipv6 incoming connections
+
+//next task->
+//now send anything(response) from server to client
+//add a feature to let server keep accepting new connection even after recieving stuff from one client
+// u can  make array with size backlog for total new socket to accept for new connections 
+//(done)u can just allocate ipv6 socket to port 8080 it will handle both  ipv4 and ipv6
+
+//long term goal -> 
+// make it accept request fro http and then respond with html file?
+//can we send and recieve files like images , videos etc 
+//make it like group chat messanger 
+
+
+
+#define _POSIX_C_SOURCE 200112L
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include<netinet/in.h>
+#include<arpa/inet.h> // For inet_pton
+#include<string.h> // For memset
+#include<unistd.h> // For close
+
+#define PORT "8080"
+#define BACKLOG 5 // How many pending connections queue will hold
+#define buff_size 100     //message buffer size
+
+
+int main(){
+    struct addrinfo hints , *servinfo, *p;
+    char buff[1024];
+    int sockfd4,sockfd6,new_sockfd, copied;
+    memset(&hints,0,sizeof(hints));
+
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags= AI_PASSIVE;
+    
+    //getting the address information
+    if(getaddrinfo(NULL,PORT,&hints,&servinfo)!=0) {
+        printf("error: getaddinfo() failed\n");
+        return 1;
+    }
+    
+    //socket creation and binding for ipv6
+    
+    for(p=servinfo;p!=NULL;p=p->ai_next) {
+        if(p->ai_family==10) {
+
+            /*printf("ip type : %d\n",p->ai_family);  // to check which ip style we got allocated
+            printf("lets see : %s\n",p->ai_next);*/
+
+            //socket for ipv6
+            sockfd6 = socket(p->ai_family,p->ai_socktype,p->ai_protocol);
+            if(sockfd6<0) {
+                printf("socket alllocation for ipv6 failed\n");
+                return 1;
+            } 
+            //binding the ipv6
+            printf("binding for ipv6 connection...\n");
+            if(bind(sockfd6,p->ai_addr,p->ai_addrlen)<0) {    //binds to all available address with ipv6 (except ipv4 too)
+                perror("bind error : ");
+                return 1;
+            }else {
+                printf("bind successfull\n");
+                break;
+            }
+        }
+    }
+
+    if(p==NULL) {
+        printf("error : no ipv6 connection found , binding failed!");
+        freeaddrinfo(servinfo);
+        close(sockfd6);
+        return 1;
+    }
+
+    //litening to upcoming connection
+    printf("server is listening to port : %s...\n",PORT);
+    if(listen(sockfd6,BACKLOG)<0) {
+        perror("listen error : ");
+        return 1;
+    }
+
+    printf("server : waiting for connection\n");
+
+    //providing new socket to connection by  accepting them
+    if((new_sockfd = accept(sockfd6,NULL,NULL))<0) {
+        perror("accept error : ");
+        return 1;
+    }
+
+    //recieving fom client
+    if((copied = recv(new_sockfd,buff,buff_size-1,0))<0) {
+        perror("recv error : ");
+        return 1;
+    }
+
+    buff[copied] ='\0';
+    printf("recieved from client : %s\n",buff);
+    //while(1);
+
+
+    freeaddrinfo(servinfo);
+    close(new_sockfd);
+    close(sockfd6);
+    return 0;
+}
+
+//ques -
+//what goes inside accept()'s 2nd and 3rd parameter 
+//why there's any need of using  getaddrinfo() for server cause server need to listen to all address not specfic
